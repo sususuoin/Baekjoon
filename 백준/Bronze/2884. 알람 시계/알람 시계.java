e4ef7f40b9dbd8bb53df1bfaaf687f33914dd4fc
sususuoin <64256411+sususuoin@users.noreply.github.com>
2022-08-10 19:27:05 +0900

    [Bronze III] Title: 알람 시계, Time: 152 ms, Memory: 16084 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException, ParseException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int hour = Integer.parseInt(st.nextToken());
+		int minute = Integer.parseInt(st.nextToken());
+		
+		if(minute<45) {
+			hour-=1;
+			minute+=15;
+			if(hour<0) {
+				hour = 23;
+			}
+		} else {
+			minute-=45;
+		}
+		System.out.println(hour + " " + minute);
+	}
+}
